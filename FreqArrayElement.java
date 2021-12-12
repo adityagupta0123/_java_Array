cca98d3c25005922479b7e3463c3b53727d0335d
ADITYA GUPTA <78367281+adityagupta0123@users.noreply.github.com>
2021-12-12 21:30:33 +0530

    FrequencyOfElement
    find the number of occur or frequency  of array digit.

---
+++ FreqArrayElement.java
@@ -0,0 +1,47 @@
+package Arrays;
+
+//import java.util.Scanner;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class DulicateNoSum {
+    public static void main(String[]args){
+
+       // Scanner sc=new Scanner(System.in);
+
+        System.out.println("enter array:");
+
+        int [] arr = new int [] {1, 2, 8, 3, 2, 2, 8, 3, 3, 3, 2, 5, 1};
+        //Array fr will store frequencies of element
+        int [] fr = new int [arr.length];
+
+        int visited = -1;
+        int count = 1;
+
+        for(int i = 0; i < arr.length; i++){
+            count=1;
+            for(int j = i+1; j < arr.length; j++){
+                if(arr[i] == arr[j]){
+                    count++;
+                    //To avoid counting same element again
+                    fr[j] = visited;
+
+                }
+            }
+            
+            if(fr[i] != visited)
+                fr[i] = count;
+        }
+
+        //Displays the frequency of each element present in array
+        System.out.println("---------------------------------------");
+        System.out.println(" Element | Frequency");
+        System.out.println("---------------------------------------");
+        for(int i = 0; i < fr.length; i++){
+            if(fr[i] != visited)
+                System.out.println("    " + arr[i] + "    |    " + fr[i]);
+        }
+        System.out.println("----------------------------------------");
+    }
+
+}
